5b3567c1ecdd93bc60d5bf6f4d58f92d33e6feb5
Nastassia Kharoshka <nasta.khar@gmail.com>
2020-02-20 16:58:03 -0500

    Compounds

--- src/main/java/com/techelevator/CampgroundCLI.java
+++ src/main/java/com/techelevator/CampgroundCLI.java
@@ -4,23 +4,35 @@ import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
+import com.techelevator.campground.model.CampgroundDAO;
+import com.techelevator.campground.model.jdbc.JDBCCampgroundDAO;
+
 public class CampgroundCLI {
 
-	public static void main(String[] args) {
+	
+		CampgroundDAO c;
+
+		
+		
+		
+		public static void main(String[] args) {
+			CampgroundCLI application = new CampgroundCLI();
+			application.run();
+	}
+
+	public CampgroundCLI() {
+		// create your DAOs here
+		
 		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setUrl("jdbc:postgresql://localhost:5432/campground");
 		dataSource.setUsername("postgres");
 		dataSource.setPassword("postgres1");
-
-		CampgroundCLI application = new CampgroundCLI(dataSource);
-		application.run();
-	}
-
-	public CampgroundCLI(DataSource datasource) {
-		// create your DAOs here
+		c = new JDBCCampgroundDAO(dataSource);
+		
 	}
 
 	public void run() {
-
+		System.out.println(c.getAllCampgroundsInPark(Long.valueOf(1)).size());
+		System.out.println("$"+c.getAllCampgroundsInPark((long) 1).get(0).getDailyFee()+"0");
 	}
 }
---
+++ src/main/java/com/techelevator/campground/model/Campground.java
@@ -0,0 +1,56 @@
+package com.techelevator.campground.model;
+
+public class Campground {
+	
+	private Long campgroundId;
+	private Long parkId;
+	private String campgroundName;
+	private String openFrom;
+	private String openTo;
+	private double dailyFee;
+	
+	
+	
+	public Long getCampgroundId() {
+		return campgroundId;
+	}
+	public void setCampgroundId(Long campgroundId) {
+		this.campgroundId = campgroundId;
+	}
+	public Long getParkId() {
+		return parkId;
+	}
+	public void setParkId(Long parkId) {
+		this.parkId = parkId;
+	}
+	public String getCampgroundName() {
+		return campgroundName;
+	}
+	public void setCampgroundName(String campgroundName) {
+		this.campgroundName = campgroundName;
+	}
+	public String getOpenFrom() {
+		return openFrom;
+	}
+	public void setOpenFrom(String openFrom) {
+		this.openFrom = openFrom;
+	}
+	public String getOpenTo() {
+		return openTo;
+	}
+	public void setOpenTo(String openTo) {
+		this.openTo = openTo;
+	}
+	public double getDailyFee() {
+		return dailyFee;
+	}
+	public void setDailyFee(double dailyFee) {
+		this.dailyFee = dailyFee;
+	}
+	
+	public String toString() {
+		return this.campgroundName;
+	}
+	
+
+}
